2f54e624566f00311cc96d6a53e1644c898e805c
Bohdan Datsiv <bohdan.datsiv@gmail.com>
2022-04-14 17:14:45 +0300

    added wait method

--- src/main/java/ua/goit/telegrambot/api/currency/FileUpdate.java
+++ src/main/java/ua/goit/telegrambot/api/currency/FileUpdate.java
@@ -2,12 +2,12 @@ package ua.goit.telegrambot.api.currency;
 
 import lombok.Getter;
 import ua.goit.telegrambot.api.currency.dto.Currency;
+import ua.goit.telegrambot.util.Utilities;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class FileUpdate extends Thread {
     private static final String ABSOLUTE_PATH = "src\\main\\resources\\Currency_rates.json";
@@ -46,45 +46,39 @@ public class FileUpdate extends Thread {
                 }
 
                 try (FileWriter writer = new FileWriter(file)) {
-                    writer.write("[\n" +
+                    writer.write("[\n" + //Create a file manually to avoid boxing and unboxing
                             "{\n" +
                             "  \"bank\": \"NBU\",\n" +
                             "  \"rateUSD\": \"" + nbuRateUSD.get(0) + "\",\n" +
                             "  \"rateEUR\": \"" + nbuRateEUR.get(0) + "\",\n" +
                             "  \"rateGBP\": \"" + nbuRateGBP.get(0) + "\"\n" +
                             "},\n" +
-                                    "{\n" +
-                                    "  \"bank\": \"Privat\",\n" +
-                                    "  \"buyUSD\": \"" + privateRateUSD.get(0) + "\",\n" +
-                                    "  \"saleUSD\": \"" + privateRateUSD.get(1) + "\",\n" +
-                                    "  \"buyEUR\": \"" + privateRateEUR.get(0) + "\",\n" +
-                                    "  \"saleEUR\": \"" + privateRateEUR.get(1) + "\"\n" +
-                                    "},\n" +
-                                            "{\n" +
-                                            "  \"bank\": \"Mono\",\n" +
-                                            "  \"buyUSD\": \"" + monoRateUSD.get(0) + "\",\n" +
-                                            "  \"saleUSD\": \"" + monoRateUSD.get(1) + "\",\n" +
-                                            "  \"buyEUR\": \"" + monoRateUSD.get(2) + "\",\n" +
-                                            "  \"saleEUR\": \"" + monoRateUSD.get(3) + "\",\n" +
-                                            "  \"CrossCurseGBP\": \"" + monoRateUSD.get(4) + "\"\n" +
-                                            "}\n" +
+                            "{\n" +
+                            "  \"bank\": \"Privat\",\n" +
+                            "  \"buyUSD\": \"" + privateRateUSD.get(0) + "\",\n" +
+                            "  \"saleUSD\": \"" + privateRateUSD.get(1) + "\",\n" +
+                            "  \"buyEUR\": \"" + privateRateEUR.get(0) + "\",\n" +
+                            "  \"saleEUR\": \"" + privateRateEUR.get(1) + "\"\n" +
+                            "},\n" +
+                            "{\n" +
+                            "  \"bank\": \"Mono\",\n" +
+                            "  \"buyUSD\": \"" + monoRateUSD.get(0) + "\",\n" +
+                            "  \"saleUSD\": \"" + monoRateUSD.get(1) + "\",\n" +
+                            "  \"buyEUR\": \"" + monoRateUSD.get(2) + "\",\n" +
+                            "  \"saleEUR\": \"" + monoRateUSD.get(3) + "\",\n" +
+                            "  \"CrossCurseGBP\": \"" + monoRateUSD.get(4) + "\"\n" +
+                            "}\n" +
                             "]");
-                    checkErr = true;
+                    checkErr = true;//
                 } catch (IOException e) {
                     System.err.println(e.getMessage());
                 }
 
-            } catch (IllegalStateException e){
+            } catch (IllegalStateException e) {
                 System.err.println(e.getMessage());
                 checkErr = false;
-            }
-            finally {
-                //delay 6 min
-                try {
-                    TimeUnit.SECONDS.sleep(360);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            } finally {
+                Utilities.wait(360);
             }
         }
     }
--- src/main/java/ua/goit/telegrambot/util/Utilities.java
+++ src/main/java/ua/goit/telegrambot/util/Utilities.java
@@ -4,11 +4,12 @@ import org.jsoup.Jsoup;
 import ua.goit.telegrambot.api.currency.FileUpdate;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public final class Utilities {
 
     //Get request from API
-    public static String getAPIRequest(String url){
+    public static String getAPIRequest(String url) {
         String json;
         try {
             json = Jsoup
@@ -25,8 +26,17 @@ public final class Utilities {
     }
 
     //check for error from API
-    public static boolean checkCurrency(){
+    public static boolean checkCurrency() {
         return FileUpdate.isCheckErr();
+    }
+
+    //wait
+    public static void wait(int seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
