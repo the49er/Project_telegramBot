3da7f914127e4f3b9b554e9d0da60e1bb204dc13
PC <dermentlif@gmail.com>
2022-04-18 17:54:37 +0200

    menu added

--- src/main/java/ua/goit/telegrambot/telegram/BotConstants.java
+++ src/main/java/ua/goit/telegrambot/telegram/BotConstants.java
@@ -5,12 +5,6 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 public class BotConstants {
 
     public static final SendMessage SEND_MESSAGE = new SendMessage();
-//    public static final String BOT_NAME = "currencyCheckerNewBot";
-//    public static final String BOT_TOKEN = "5106700465:AAGKyVPVsM06SPfllAporcZy_4qF8Q0Jvf0";
-
-    public static final String BOT_NAME = "telegram_bot_test_goit5";
-    public static final String BOT_TOKEN = "5377167528:AAEQDTHvVQIQuduytc0SElzp68zTExJ3MTk";
-
-
-
+    public static final String BOT_NAME = "currencyCheckerNewBot";
+    public static final String BOT_TOKEN = "5106700465:AAGKyVPVsM06SPfllAporcZy_4qF8Q0Jvf0";
 }
