9f3e5ab813e58e5cfe1787647bea5e6580e5259d
Morozov Artem <artem.morozov2004ma@gmail.com>
2022-04-21 11:10:01 +0300

    Really working notifications(must be..)

--- src/main/java/ua/goit/telegrambot/settings/Scheduler.java
+++ src/main/java/ua/goit/telegrambot/settings/Scheduler.java
@@ -7,6 +7,7 @@ import org.checkerframework.checker.units.qual.C;
 import ua.goit.telegrambot.telegram.TelegramCurrencyBot;
 
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 
@@ -22,20 +23,17 @@ public class Scheduler implements Runnable {
         TelegramCurrencyBot bot = new TelegramCurrencyBot();
 
 
-        if (calendar.get(Calendar.HOUR_OF_DAY) >= 0 && calendar.get(Calendar.HOUR_OF_DAY) < 8) {
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        if (hour <9 || hour > 18) {
+            log.info("thread to sleep");
+            if(hour>=18) {
+                calendar.add(Calendar.DAY_OF_MONTH, 1);
+            }
             calendar.set(Calendar.HOUR_OF_DAY, 9);
             calendar.set(Calendar.MINUTE, 0);
             calendar.set(Calendar.SECOND, 0);
-            Long date = calendar.getTimeInMillis();
-            Long delay = date - System.currentTimeMillis();
-            Thread.sleep(delay);
-        } else if (calendar.get(Calendar.HOUR_OF_DAY) > 18 && calendar.get(Calendar.HOUR_OF_DAY) < 24) {
-            calendar.add(Calendar.DAY_OF_MONTH, 1);
-            calendar.set(Calendar.HOUR_OF_DAY, 9);
-            calendar.set(Calendar.MINUTE, 0);
-            calendar.set(Calendar.SECOND, 0);
-            Long date = calendar.getTimeInMillis();
-            Long delay = date - System.currentTimeMillis();
+            Date date1 = calendar.getTime();
+            long delay = date1.getTime() - System.currentTimeMillis()+5000;
             Thread.sleep(delay);
         }
 
@@ -43,28 +41,31 @@ public class Scheduler implements Runnable {
         UserService service = UserService.getInstance();
 
         while (true) {
+            log.info("thread to notify");
             calendar = new GregorianCalendar();
-            int time = calendar.get(Calendar.HOUR_OF_DAY);
-            List<Integer> userIds = service.getUsersWithNotficationOnCurrentHour(time);
+            hour = calendar.get(Calendar.HOUR_OF_DAY);
+            List<Integer> userIds = service.getUsersWithNotficationOnCurrentHour(hour);
 
             for (Integer userId : userIds) {
                 bot.sendNotification(Long.valueOf(userId));
                 log.info("sent notification");
             }
-            if (time >= 9 && time <= 17) {
-                calendar.add(Calendar.HOUR_OF_DAY, 1);
-                Long addHour = calendar.getTimeInMillis();
-                Long delay = addHour - System.currentTimeMillis();
-                Thread.sleep(delay);
-            } else if (time >= 18) {
-                calendar.add(Calendar.DAY_OF_MONTH, 1);
-                calendar.set(Calendar.HOUR_OF_DAY, 9);
-                calendar.set(Calendar.MINUTE, 0);
-                calendar.set(Calendar.SECOND, 0);
-                Long date = calendar.getTimeInMillis();
-                Long delay = date - System.currentTimeMillis();
-                Thread.sleep(delay);
+            if (hour >= 18){
+                calendar.add(Calendar.HOUR_OF_DAY,1);
             }
+            if (hour < 8){
+                calendar.set(Calendar.HOUR_OF_DAY,9);
+                calendar.set(Calendar.MINUTE,0);
+            }else if (hour < 18){
+                calendar.set(Calendar.HOUR_OF_DAY,hour + 1);
+                calendar.set(Calendar.MINUTE,0);
+            }else {
+                calendar.set(Calendar.HOUR_OF_DAY,9);
+                calendar.set(Calendar.MINUTE,0);
+            }
+            long timeInMillis = calendar.getTimeInMillis();
+            long delay = timeInMillis - System.currentTimeMillis();
+            Thread.sleep(delay);
         }
     }
 }
--- src/main/java/ua/goit/telegrambot/telegram/BotConstants.java
+++ src/main/java/ua/goit/telegrambot/telegram/BotConstants.java
@@ -10,6 +10,6 @@ public class BotConstants {
     //public static final String BOT_TOKEN = "5106700465:AAGKyVPVsM06SPfllAporcZy_4qF8Q0Jvf0"; //#1
     //public static final String BOT_TOKEN = "5116828904:AAF2pIBVm_yUjSG4QIQAylUv6PlMHjajqPc"; //#2
 
-    public static final String BOT_NAME = "@DoSomeoneReallyCare_bot";
-    public static final String BOT_TOKEN = "5116828904:AAF2pIBVm_yUjSG4QIQAylUv6PlMHjajqPc";
+    public static final String BOT_NAME = "@GoitJavaMorozovCurrencyBot";
+    public static final String BOT_TOKEN = "5278898591:AAGMwf2MbA66kt1UB75rejj8ZqLoRcSzthg";
 }
